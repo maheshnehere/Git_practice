c27a4d3298f9932dd93dc4accf842391becb68a8
maheshnehere <maheshnehere93@gmail.com>
2024-02-09 16:59:28 +0530

    made changes to main2

--- src/main2.java
+++ src/main2.java
@@ -1,5 +1,14 @@
+import java.util.Enumeration;
+import java.util.Scanner;
+
 public class main2 {
     public static void main(String[] args) {
-        System.out.println(" I m from main 2 & two");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("I am main 2 class");
+        }
     }
 }
